2a4229edec9296c0dd9655234601a67eb1d35ee4
Jeffrey Yu <jwy5140@gmail.com>
2019-02-23 09:13:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,14 @@
 def key_for_min_value(name_hash)
   arr = name_hash.collect {|x,y| y}
   arr2 = name_hash.collect {|x,y| x}
+  
+  while arr.length > 0 do 
+    if arr.first > arr.last 
+      arr.pop
+      arr2.pop
+    else
+      arr.shift
+      arr2.shift
+    end
+  arr2[0]
 end
