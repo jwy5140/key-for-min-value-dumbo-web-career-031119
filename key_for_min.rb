a38470dcce7f760d75484c4476dab093e0aa6a36
Jeffrey Yu <jwy5140@gmail.com>
2019-02-23 09:17:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   arr = name_hash.collect {|x,y| y}
   arr2 = name_hash.collect {|x,y| x}
-  while arr.length > 0 do 
+  while arr.length > 1 do 
     if arr.first > arr.last 
       arr.shift
       arr2.shift
@@ -13,6 +13,5 @@ def key_for_min_value(name_hash)
       arr2.pop
     end
   end
-  binding.pry
   arr2[0]
 end
