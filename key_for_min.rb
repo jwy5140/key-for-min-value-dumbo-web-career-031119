db07f2ba02590ce24d5885f3422fe14534625d70
Jeffrey Yu <jwy5140@gmail.com>
2019-02-23 09:11:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,5 @@
 
 def key_for_min_value(name_hash)
   arr = name_hash.collect {|x,y| y}
-  name_hash.each {|x,y|
-  if arr[0] < arr[1]
-    name_hash.delete(x)
-    arr.shift
-  else
-    arr.delete_at(1)
-  end
-  }
-  name_hash.index(arr[0])  
+  arr2 = name_hash.collect {|x,y| x}
 end
