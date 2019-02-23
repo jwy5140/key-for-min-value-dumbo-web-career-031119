841c6c8ba2e7b786a22d6bfc168f6e36b24c68ef
Jeffrey Yu <jwy5140@gmail.com>
2019-02-23 09:11:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  arr = name_hash.collect {|x,y| y}
+  name_hash.each {|x,y|
+  if arr[0] < arr[1]
+    name_hash.delete(x)
+    arr.shift
+  else
+    arr.delete_at(1)
+  end
+  }
+  name_hash.index(arr[0])  
 end
